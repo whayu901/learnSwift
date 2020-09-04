c8ed741778b530c00c27080ea233cf14d32cebcb
wahyu setiawan <wahyu@gradana.com>
2020-09-05 06:20:17 +0700

    Initial Commit

---
+++ learnApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  learnApp
+//
+//  Created by GRADANA on 05/09/20.
+//  Copyright © 2020 GRADANA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, inggrid!")
+        
+//        Text("Hello, wahyu")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
