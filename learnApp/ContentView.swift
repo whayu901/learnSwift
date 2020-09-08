d98f3a6fdf009028b6d5a6e034aab51fcf56a2c9
wahyu setiawan <wahyu@gradana.com>
2020-09-08 17:05:52 +0700

    perubahan nama

--- learnApp/ContentView.swift
+++ learnApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, inggrid!")
+        Text("Hello, inggrid!").bold()
         
 //        Text("Hello, wahyu")
     }
